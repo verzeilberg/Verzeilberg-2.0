34fb6e3e02c791ef9ea6fd8e5c9911ef3eee844e
“Sander” <sander@verzeilberg.nl>
2023-01-06 16:47:48 +0100

    First commit

---
+++ config/modules.config.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Cache',
+    'Laminas\Paginator',
+    'Laminas\Mvc\Plugin\FilePrg',
+    'Laminas\Mvc\Plugin\FlashMessenger',
+    'Laminas\Mvc\Plugin\Identity',
+    'Laminas\Mvc\Plugin\Prg',
+    'Laminas\Session',
+    'Laminas\Mvc\Middleware',
+    'Laminas\Mvc\I18n',
+    'Laminas\Form',
+    'Laminas\I18n',
+    'Laminas\Log',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Hydrator',
+    'Laminas\Di',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Laminas\DeveloperTools',
+    'Laminas\Diactoros',
+    'DoctrineModule',
+    'DoctrineORMModule',
+    'Application',
+];
