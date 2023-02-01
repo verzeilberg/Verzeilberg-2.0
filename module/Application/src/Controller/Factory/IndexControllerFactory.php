550336c1e2484134a58a60bc09aa5f558987eb20
“Sander” <sander@verzeilberg.nl>
2023-02-01 08:20:36 +0100

    Changed for phph 8.1 and layout

--- module/Application/src/Controller/Factory/IndexControllerFactory.php
+++ module/Application/src/Controller/Factory/IndexControllerFactory.php
@@ -2,21 +2,49 @@
 
 namespace Application\Controller\Factory;
 
+use Blog\Service\blogService;
+use Event\Service\eventService;
 use Laminas\ServiceManager\Factory\FactoryInterface;
-use Laminas\ServiceManager\ServiceLocatorInterface;
 use Interop\Container\ContainerInterface;
 use Application\Controller\IndexController;
+use StravaApi\Entity\Activity;
+use StravaApi\Entity\ActivityImportLog;
+use StravaApi\Entity\Round;
+use StravaApi\Service\StravaService;
+use Twitter\Service\twitterOathService;
+use Twitter\Service\twitterService;
 
-class IndexControllerFactory implements FactoryInterface {
-
-    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+class IndexControllerFactory implements FactoryInterface
+{
+    /**
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \ErrorException
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null): IndexController
     {
-        $entityManager = $container->get('doctrine.entitymanager.orm_default');
-        $viewHelperManager = $container->get('ViewHelperManager');
+        $entityManager                  = $container->get('doctrine.entitymanager.orm_default');
+        $blogService                    = new blogService($entityManager);
+        $eventService                   = new eventService($entityManager);
+        $config                         = $container->get('config');
+        $activityRepository             = $entityManager->getRepository(Activity::class);
+        $roundRepository                = $entityManager->getRepository(Round::class);
+        $activityImportLogRepository    = $entityManager->getRepository(ActivityImportLog::class);
+        $stravaService                  = new StravaService(
+            $config,
+            $activityRepository,
+            $roundRepository,
+            $activityImportLogRepository
+        );
+        $twitterService = new twitterService($config);
+        $twitterOathService = new twitterOathService($config, $twitterService);
+
         return new IndexController(
-            $entityManager,
-            $viewHelperManager
+            $blogService,
+            $eventService,
+            $stravaService,
+            $twitterService,
+            $twitterOathService
         );
     }
-
 }
--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -4,13 +4,73 @@ declare(strict_types=1);
 
 namespace Application\Controller;
 
+use Blog\Service\blogService;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Event\Service\eventService;
+use Exception;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
+use StravaApi\Service\StravaService;
+use Twitter\Service\twitterOathService;
+use Twitter\Service\twitterService;
 
 class IndexController extends AbstractActionController
 {
-    public function indexAction()
+    /** @var blogService */
+    protected $blogService;
+    /** @var eventService */
+    protected $eventService;
+    /** @var StravaService */
+    protected $stravaService;
+    /** @var twitterService */
+    protected $twitterService;
+    /** @var twitterOathService  */
+    protected $twitterOathService;
+
+    public function __construct(
+        blogService $blogService,
+        eventService $eventService,
+        stravaService $stravaService,
+        twitterService $twitterService,
+        twitterOathService $twitterOathService
+    ) {
+        $this->blogService          = $blogService;
+        $this->eventService         = $eventService;
+        $this->stravaService        = $stravaService;
+        $this->twitterService       = $twitterService;
+        $this->twitterOathService   = $twitterOathService;
+    }
+
+    /**
+     * @throws NonUniqueResultException
+     * @throws NoResultException
+     * @throws Exception
+     */
+    public function indexAction(): ViewModel
     {
-        return new ViewModel();
+        //Get blogs
+        $blogs = $this->blogService->getOnlineBlogsBasedOnStartAndOffSet(0, 6);
+        //Get events
+        $events         = $this->eventService->getUpcommingEvent(3);
+
+        $upcommingEvent = $this->eventService->getUpcommingEvent();
+
+        //Tweets
+        $tweets = $this->twitterOathService->getTwitterUserTimeline(1, 4);
+        $tweets = $this->twitterService->createTweetArray($tweets);
+
+        return new ViewModel([
+            'blogs'                 => $blogs,
+            'events'                => $events,
+            'totalRunActivities'    => $this->stravaService->activityRepository->getTotalItems('Run'),
+            'totalRunDistance'      => $this->stravaService->activityRepository->getTotalDistance('Run'),
+            'totalRunTime'          => $this->stravaService->activityRepository->getTotalTime('Run'),
+            'averageSpeed'          => $this->stravaService->activityRepository->getAverageSpeed('Run'),
+            'averageElevation'      => $this->stravaService->activityRepository->getAverageElevation('Run'),
+            'averageHeartbeat'      => $this->stravaService->activityRepository->getAverageHeartbeat('Run'),
+            'tweets'                => $tweets,
+            'upcommingEvent'        => $upcommingEvent,
+        ]);
     }
 }
